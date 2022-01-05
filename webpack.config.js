3f514ae8df4436e0dfc56a9918c5ee3e33d64e57
Abdyl-Aziz <azizshihabov@mail.ru>
2022-01-05 22:26:01 +0300

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssnanoPlugin = require('cssnano-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './src')
+    },
+    hot: true,
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+
+  module: {
+    rules: [{
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: "defaults"
+              }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+
+  optimization: {
+    minimizer: [
+      new CssnanoPlugin(),
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  }
+};
