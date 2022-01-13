b9a0a91bb312e5924db024452e6f9189e6addfac
Abdyl-Aziz <azizshihabov@mail.ru>
2022-01-13 18:24:51 +0300

    feat: add posts

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { NavigationComponent } from "./components/navigation.component";
 import {FavoriteComponent} from './components/favorite.component';
 import {CreateComponent} from './components/create.component';
 import {PostsComponent} from './components/posts.component';
+import {LoaderComponent} from './components/loader.component';
 
 new HeaderComponent('header');
 
-
+const loader = new LoaderComponent('loader');
 const navigation = new NavigationComponent('navigation');
-const posts = new PostsComponent('posts');
+const posts = new PostsComponent('posts', {loader});
 const create = new CreateComponent('create');
 const favorite = new FavoriteComponent('favorite');
 
